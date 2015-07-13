90bfad2c3940222e7a82d6f63d8cbc6068f4ed09
johnny lamb <agnusjack@gmail.com>
2015-07-13 11:03:14 -0600

    code for return_vs_console

---
+++ main.js
@@ -0,0 +1,42 @@
+// //functions
+//
+//
+//
+// //function expression syntax
+// var greet = function() {
+//
+//   console.log("hello ");
+// }
+//
+// greet();
+//
+//
+// ////function declaration syntax
+// function greetDeclarationSyntax(){
+//
+//   console.log("hello 2");
+//
+// }
+//
+// greetDeclarationSyntax();
+//
+//
+//
+// // returning values
+//
+// function classGreetingReturn(studentName){
+//   return "hi, " + studentName;
+//
+//
+// }
+// classGreetingReturn('brandon');
+function square(x){
+
+  return x * x;
+}
+
+var result = square(4);
+console.log(result);
+
+console.log(square(4));
+square(4);
